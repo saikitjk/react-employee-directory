f5dfe938deb35c83ca67ba112bc4429b0ff152e2
JK <saikitjk@gmail.com>
2020-10-30 22:55:02 -0700

    created header

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,6 +1,6 @@
 //import logo from "./logo.svg";
 import "./App.css";
-import Header from "./components/header/header";
+import Header from "./components/header/index";
 
 function App() {
   return (
---
+++ employee-directory/src/components/header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export default function Header() {
+  return (
+    <div classname="header">
+      <h1>Employee Directory</h1>
+    </div>
+  );
+}
