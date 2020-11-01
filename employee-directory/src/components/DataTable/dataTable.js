4687d28fcdefde32e49b63dd83ab954412de8817
JK <saikitjk@gmail.com>
2020-10-31 22:02:31 -0700

    created data table

--- employee-directory/src/components/DataTable/dataTable.js
+++ employee-directory/src/components/DataTable/dataTable.js
@@ -1,10 +1,43 @@
 import React from "react";
 import "../UserCard/userCard";
 
-export default function dataTable() {
+export default function dataTable(props) {
   return (
     <>
-      <div></div>
+      <table className="empTable">
+        <thead>
+          <tr>
+            <th></th>
+            <th onClick={props.sortByName}>Name</th>
+            <th>Phone</th>
+            <th>E-mail</th>
+            <th>DOB</th>
+          </tr>
+        </thead>
+        <tbody className="tableBody">
+          {props.results.map((result) => (
+            <tr className="table" key={result.login.uuid}>
+              <td>
+                {" "}
+                <img
+                  className="
+                        "
+                  src={result.picture.medium}
+                  alt=""
+                />
+              </td>
+
+              <td>{result.name.first + " " + result.name.last} </td>
+
+              <td>{result.cell}</td>
+              <td className="email">
+                <a href={result.email}>{result.email}</a>
+              </td>
+              <td>{result.dob.date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
